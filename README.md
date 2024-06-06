62ef1b5876a6ef426631dff8adefb7e6b1cdac89
Winston Pacheco Junior <winston@atleastonce.cc>
2024-06-05 22:00:58 -0300

    fetching data from awesome api and returning as json

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Os requisitos para cumprir este desafio são:
   
 [X] O endpoint necessário gerado pelo server.go para este desafio será: /cotacao e a porta a ser utilizada pelo servidor HTTP será a 8080.
   
-[ ] O server.go deverá consumir a API contendo o câmbio de Dólar e Real no endereço: https://economia.awesomeapi.com.br/json/last/USD-BRL e    
-[ ] em seguida deverá retornar no formato JSON o resultado para o cliente.  
+[X] O server.go deverá consumir a API contendo o câmbio de Dólar e Real no endereço: https://economia.awesomeapi.com.br/json/last/USD-BRL e    
+[X] em seguida deverá retornar no formato JSON o resultado para o cliente.  
   
 [ ] Usando o package "context", o server.go deverá registrar no banco de dados SQLite cada cotação recebida,  
 [ ] sendo que o timeout máximo para chamar a API de cotação do dólar deverá ser de 200ms e o  
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
@@ -15,5 +16,29 @@ func cotacao(w http.ResponseWriter, r *http.Request) {
 	log.Println("/cotacao request received")
 	defer log.Println("/cotacao request completed")
 
-	w.Write([]byte("Hello World"))
+	ctx := r.Context()
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
 }
